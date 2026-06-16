f6ca45a7550804676956b898c7201eb1a6087fbf
Ruizhi Zhong <ruizhi.zhong@outlook.com>
2026-06-16 23:10:50 +0800

    fix: install indicator package structure for KF6

--- autotests/packageunittest.cpp
+++ autotests/packageunittest.cpp
@@ -6,7 +6,10 @@
 #include "lattepackage.h"
 
 #include <KPackage/Package>
+#include <KPackage/PackageLoader>
 
+#include <QDir>
+#include <QTemporaryDir>
 #include <QTest>
 
 class PackageUnitTest : public QObject
@@ -15,6 +18,7 @@ class PackageUnitTest : public QObject
 
 private Q_SLOTS:
     void lattePackageResolvesShellDefinitions();
+    void indicatorPackageLoadsFromPluginAndResolvesDirectories();
 };
 
 void PackageUnitTest::lattePackageResolvesShellDefinitions()
@@ -27,6 +31,33 @@ void PackageUnitTest::lattePackageResolvesShellDefinitions()
     QVERIFY(package.filePath("configmodel").endsWith(QStringLiteral("configuration/config.qml")));
     QVERIFY(package.filePath("templates").endsWith(QStringLiteral("templates")));
     QVERIFY(package.filePath("compactapplet").endsWith(QStringLiteral("applet/CompactApplet.qml")));
+}
+
+void PackageUnitTest::indicatorPackageLoadsFromPluginAndResolvesDirectories()
+{
+    QTemporaryDir dir;
+    QVERIFY(dir.isValid());
+
+    const QStringList directories{
+        QStringLiteral("config"),
+        QStringLiteral("ui"),
+        QStringLiteral("data"),
+        QStringLiteral("code"),
+        QStringLiteral("locale")
+    };
+
+    for (const QString &directory : directories) {
+        QVERIFY(QDir(dir.path()).mkpath(QStringLiteral("contents/") + directory));
+    }
+
+    KPackage::Package package = KPackage::PackageLoader::self()->loadPackage(QStringLiteral("Latte/Indicator"));
+    package.setPath(dir.path());
+
+    QVERIFY(package.filePath("config").endsWith(QStringLiteral("config")));
+    QVERIFY(package.filePath("ui").endsWith(QStringLiteral("ui")));
+    QVERIFY(package.filePath("data").endsWith(QStringLiteral("data")));
+    QVERIFY(package.filePath("scripts").endsWith(QStringLiteral("code")));
+    QVERIFY(package.filePath("translations").endsWith(QStringLiteral("locale")));
 }
 
 QTEST_GUILESS_MAIN(PackageUnitTest)
